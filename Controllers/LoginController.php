2c8fd58b1f9bb389296a97a054489a9faa1dee03
Igor Simões da Silveira <igor01silveira@gmail.com>
2019-03-25 04:44:04 -0300

    Instalando o AdminLTE, Iniciando o Login no painel administrativo e criando Layout de login

---
+++ Controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Controllers;
+
+use \Core\Controller;
+use \Models\Users;
+
+class LoginController extends Controller {
+
+	public function index() {
+		$array = array();
+
+		
+		$this->loadView('login', $array);
+	}
+
+}
